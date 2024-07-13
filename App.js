b291db25c1454c096e496e577afaea7b1c76fc36
gyulls2 <kuri6233@gmail.com>
2024-07-14 01:46:21 +0900

    Location

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
 import { Text, View, StyleSheet, ScrollView, Dimensions } from "react-native";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+
+  const ask = async () => {
+    // 권한 요청은 앱을 실행할 때 한 번만 요청하면 된다.
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+
+    // 권한이 허용되지 않았을 때
+    if (!granted) {
+      setOk(flase);
+    }
+
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+
+    setCity(location[0].city);
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         pagingEnabled
         horizontal
         showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.weather}
-      >
+        contentContainerStyle={styles.weather}>
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
