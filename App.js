3653db750f16e3f8abfcae9488912ae9c9db6264
gyulls2 <kuri6233@gmail.com>
2024-07-14 01:14:15 +0900

    Styles

--- App.js
+++ App.js
@@ -1,25 +1,67 @@
-// import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Text, View, StyleSheet, ScrollView, Dimensions } from "react-native";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
   return (
-    <View style={{flex: 1, flexDirection:"row"}}>
-      <View style={{flex:1, backgroundColor:"tomato"}}></View>
-      <View style={{flex:3, backgroundColor:"teal"}}></View>
-      <View style={{flex:1, backgroundColor:"orange"}}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#000",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-//   text: {
-//     fontSize: "30px",
-//     color: "#fff",
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {
+    //contentContainerStyle는 flex를 사용하지 않는다.
+    // flex: 3,
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  description: { fontSize: 60, marginTop: -30 },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+});
